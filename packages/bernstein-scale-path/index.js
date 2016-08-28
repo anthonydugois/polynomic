f9345b70945f03e2372a1d9310cb62ce12de6ca5
Anthony Dugois <dev.anthonydugois@gmail.com>
2016-08-28 15:32:49 +0200

    No need of bernstein-core anymore

--- packages/bernstein-scale-path/index.js
+++ packages/bernstein-scale-path/index.js
@@ -1,6 +1,11 @@
 import matrixOrigin from "bernstein-matrix-origin-path"
 
 export default function scale(path, sx, sy, ...origin) {
+  // uniform scaling
+  if (typeof sy === "undefined") {
+    sy = sx
+  }
+
   return matrixOrigin(path, [
     sx, 0, 0,
     0, sy, 0,
