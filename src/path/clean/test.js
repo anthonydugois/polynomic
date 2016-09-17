86606afbce44e4f99ae618efa109c68e875cb7d3
Anthony Dugois <dev.anthonydugois@gmail.com>
2016-09-17 15:27:37 +0200

    Setup jest

--- src/path/clean/test.js
+++ src/path/clean/test.js
@@ -1,14 +1,11 @@
-import { assert } from "chai"
 import parse from "../../pathstring/parse"
 import isEqual from "../is-equal"
 import clean from "./index"
 
-describe("path/clean", function () {
-  it("should clean the invalid path", function () {
-    const path = parse("L0 0l50 50l0 0h50v50 L0 0")
-    const test = clean(path)
-    const expected = "M0 0l50 50h50v50z"
+test("should clean the invalid path", () => {
+  const path = parse("L0 0l50 50l0 0h50v50 L0 0")
+  const test = clean(path)
+  const expected = "M0 0l50 50h50v50z"
 
-    assert.isTrue(isEqual(test, expected))
-  })
+  expect(isEqual(test, expected)).toBe(true)
 })
