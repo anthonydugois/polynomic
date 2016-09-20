a8090dddfa0f090212c1d84e19cc01ad23f3c75a
Anthony Dugois <dev.anthonydugois@gmail.com>
2016-09-20 17:19:43 +0200

    Handle rounded corners on rect

--- src/path/from-rect/index.js
+++ src/path/from-rect/index.js
@@ -1,5 +1,5 @@
 import { xmlns } from "../../constants"
-import { M, L, Z } from "../../point/points"
+import { M, L, A, Z } from "../../point/points"
 
 export default function fromRect(node) {
   if (node.nodeName.toLowerCase() !== "rect") {
@@ -10,14 +10,33 @@ export default function fromRect(node) {
   const y = parseFloat(node.getAttributeNS(xmlns, "y"))
   const width = parseFloat(node.getAttributeNS(xmlns, "width"))
   const height = parseFloat(node.getAttributeNS(xmlns, "height"))
+  const rx = parseFloat(node.getAttributeNS(xmlns, "rx"))
+  const ry = parseFloat(node.getAttributeNS(xmlns, "ry"))
 
-  const first = M(x, y)
+  if (isNaN(rx) && isNaN(ry)) {
+    const first = M(x, y)
+
+    return [
+      first,
+      L(x + width, y),
+      L(x + width, y + height),
+      L(x, y + height),
+      Z(first),
+    ]
+  }
+
+  const first = M(x + rx, y)
 
   return [
     first,
-    L(x + width, y),
-    L(x + width, y + height),
-    L(x, y + height),
+    L(x + width - rx, y),
+    A(rx, ry, 0, 0, 1, x + width, y + ry),
+    L(x + width, y + height - ry),
+    A(rx, ry, 0, 0, 1, x + width - rx, y + height),
+    L(x + rx, y + height),
+    A(rx, ry, 0, 0, 1, x, y + height - ry),
+    L(x, y + ry),
+    A(rx, ry, 0, 0, 1, x + rx, y),
     Z(first),
   ]
 }
--- src/path/from-rect/test.js
+++ src/path/from-rect/test.js
@@ -15,3 +15,19 @@ test("should get the corresponding path from the SVG rect node", () => {
 
   expect(isEqual(test, expected)).toBe(true)
 })
+
+test("should get the corresponding path from the rounded SVG rect node", () => {
+  const node = document.createElementNS(xmlns, "rect")
+
+  node.setAttributeNS(xmlns, "x", 0)
+  node.setAttributeNS(xmlns, "y", 0)
+  node.setAttributeNS(xmlns, "width", 100)
+  node.setAttributeNS(xmlns, "height", 100)
+  node.setAttributeNS(xmlns, "rx", 10)
+  node.setAttributeNS(xmlns, "ry", 10)
+
+  const test = fromRect(node)
+  const expected = "M10 0L90 0A10 10 0 0 1 100 10L100 90A10 10 0 0 1 90 100L10 100A10 10 0 0 1 0 90L0 10A10 10 0 0 1 10 0Z"
+
+  expect(isEqual(test, expected)).toBe(true)
+})
